d96979e057bd15fedb1c45eb42318da930fcac66
lattechiffon <lattechiffon@gmail.com>
2017-11-27 20:36:16 +0900

    UI_AbilitySystem
    Ability System Upgrade

---
+++ Assets/Script/Manager/Scene/AbilitySystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilitySystem : MonoBehaviour {
+
+	public void GetAbilityPoint() {
+		// 어빌리티 포인트 게터 구현
+	}
+
+	public void SetAttackPoint(int delta) {
+		// 공격력
+		print("공격력");
+	}
+
+	public void SetDefencePoint(int delta) {
+		// 방어력
+		print("방어력");
+	}
+
+	public void SetHealthPoint(int delta) {
+		// 체력
+		print("체력");
+	}
+}
